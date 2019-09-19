3a09543848406b3d5e95a8446c996405e42e7f68
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-19 17:35:42 +0900

    インスタンスメソッドの学習
    - インスタンスのメソッドを作成することで、処理をまとめることができる。

--- person.py
+++ person.py
@@ -1,23 +1,33 @@
 class Person:
-    def __init__(self, name, age):   # argument：引数
+    def __init__(self, name, age):  # argument：引数
         # self.name は インスタンス変数(プロパティ/Field/Attribute)という
         self.name = name
         self.age = age
 
+    def info(self):
+        return f'{self.name}は{self.age}歳です'
 
 
 def main():
     # Personクラスをインスタンス化して、そのインスタンスをbobと名付けた
     bob = Person(name='Bob', age=18)
-    print(bob)
-    print(bob.name)
-    print(bob.age)
+    # print(bob)
+    # print(bob.name)
+    # print(bob.age)
 
     # Personクラスをインスタンス化して、そのインスタンスをtomと名付ける
     tom = Person(name='Tom', age=30)
-    print(tom)
-    print(tom.name)
-    print(tom.age)
+    # print(tom)
+    # print(tom.name)
+    # print(tom.age)
+
+    # Bobは18歳です　と出力したい
+    print(f'{bob.name}は{bob.age}歳です')
+    print(f'{tom.name}は{tom.age}歳です')
+
+    # 名前と年齢を出力する手続きをまとめたい
+    print(bob.info())
+    print(tom.info())
 
 if __name__ == '__main__':
     main()
